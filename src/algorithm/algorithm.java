11fabb343ee895ccda2578177b7497052ba62d17
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-17 00:25:08 +0200

    removed terminology from algorithms file, it is covered in readme

--- src/algorithm/algorithm.java
+++ src/algorithm/algorithm.java
@@ -8,27 +8,9 @@ import java.util.Map;
  * Part of randomProjections, in package algorithm.
  */
 public class algorithm {
-    /** Terminology:
-     * data: Long string containing instances of planted or real motifs, flawed by up to a specified number of differences from the model.
-     * n: Length of data
-     * Motif: recurring contiguous subsequence in data, containing some noise.
-     * L: Length of motif searched for.
-     *
-     * |c|, #c: cardinality of datastructure/collection c.
-     * Noise, differences, distortions: mismatches of model and instance of motif.
-     * support of a motif/model: Amount of occurrences of motif/model in data.
-     *
-     * ĸ-scatter of s: String that is concatenation of characters s[i] where i in ĸ. ĸ is a collection of integers.
-     'Mathematical' notation: [ s[i] | i in ĸ]. Length of ĸ-scatter: |k|.
-     * k: Array or collection containing up to L-d random indices where L is the length of the motif and d is the error tolerance.
-     If there were more indices, the k-scatter and the set of indices of deviations in a motif could never be disjoint.
-     The indices can be in the range [0, L-1].
-
-     * Friend: 2 strings a and  b in data are defined to be friends if they share some k-scatters (i.e. if for some random projections, the scatters of a and b are equal).
-     */
-
-    /**
+   /**
      * Random Projections algorithm
+    * @see README.md for terminology and paper.
      * Takes a reference to data, a motif length and the amount of permitted errors.
      *
      * @param data            Long string containing instances of planted or real motifs, flawed by up to {@code permittedErrors} differences from the model.
@@ -36,6 +18,7 @@ public class algorithm {
      * @param permittedErrors Allowed distance of an instance of a motif from the implicit model
      * @return Returns a mapping between motifs and their support
      */
+    @SuppressWarnings("JavadocReference")
     public static Map<String, Integer> randomProjections(String data, int motifLength, int permittedErrors) {
         return null;
     }
