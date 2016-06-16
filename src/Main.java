681f603965d9c5b6f3fdf440126daa8afe276820
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-16 20:59:25 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import static algorithm.algorithm.randomProjections;
+
+/**
+ * A motif is understood as a reappearing sequence.
+ * Part of randomProjections, in package src.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // motifs of length 4 might be: some, byte, (sequ, eque, ..., ence)
+        // somesequence is a motif of length 4 even if it is obfuscated a little
+        String data = "someseruenceterabytesomtsequencenoisebiterepeatsomesequencdwhatisbyttspace somezequencequencesomesaucegingersomesequebcebyteis8bitsomesequence";
+        randomProjections(data, 4, 1);
+    }
+}
---
+++ src/algorithm/algorithm.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+import javafx.util.Pair;
+
+import java.util.List;
+
+/**
+ * Created by ra on 16.06.16.
+ * Part of randomProjections, in package algorithm.
+ */
+public class algorithm {
+    public static Pair<Integer, List<String>> randomProjections(String data, int motifLength, int permittedErrors) {
+        return null;
+    }
+}
