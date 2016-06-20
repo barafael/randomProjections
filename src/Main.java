69885dd8296d7d5975d72100e58e4bc12dd48b1f
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-20 23:03:13 +0200

    delete test file-unused.

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,12 @@ import static algorithm.RandomProjections.randomProjections;
  * A motif is understood as a reappearing sequence.
  * Part of randomProjections, in package src.
  */
-public class Main {
+class Main {
     public static void main(String[] args) {
         // motifs of length 4 might be: some, byte, (sequ, eque, ..., ence)
         // somesequence is a motif of length 4 even if it is obfuscated a little
-        String data = "someseruenceterabytesomtsequencenoisebiterepeatsomesequencdwhatisbyttspace somezequencequencesomesaucegingersomesequebcebyteis8bitsomesequence";
-        randomProjections(data, 10, 3, 3, 2);
+        // String data = "someseruenceterabytesomtsequencenoisebiterepeatsomesequencdwhatisbyttspace somezequencequencesomesaucegingersomesequebcebyteis8bitsomesequence";
+        // randomProjections(data, 10, 3, 3, 2);
         try {
             String preludio = readFile("../midiParser/assets/midi/csv/preludioC.mid/tracks/track0.csv", 1);
             randomProjections(preludio, 12, 3, 4, 3);
@@ -32,7 +32,6 @@ public class Main {
             String[] cols = line.split(", ");
             if (cols.length > column) {
                 parsedColumn.add(line.split(", ")[column]);
-                System.out.println(line.split(", ")[column]);
             }
         }
         return parsedColumn.stream().reduce("", (String::concat));
