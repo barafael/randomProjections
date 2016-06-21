9cc3a7c4a15742afa06413194425171cee390f91
Rafael Bachmann <triolenzunge@gmail.com>
2016-06-21 23:22:52 +0200

    swap argument order to avoid overlooking

--- random_projections/Main.java
+++ random_projections/Main.java
@@ -15,17 +15,18 @@ class Main {
     public static void main(String[] args) {
         // motifs of length 4 might be: some, byte, (sequ, eque, ..., ence)
         // somesequence is a motif of length 4 even if it is obfuscated a little
-        // String data = "someseruenceterabytesomtsequencenoisebiterepeatsomesequencdwhatisbyttspace somezequencequencesomesaucegingersomesequebcebyteis8bitsomesequence";
+        // String data = "someseruenceterabytesomtsequencenoisebiterepeatsomesequencdwhatisbyttspace " +
+        // "somezequencequencesomesaucegingersomesequebcebyteis8bitsomesequence";
         // randomProjections(data, 10, 3, 3, 2);
         try {
-            String preludio = readFile("../midiParser/assets/midi/csv/preludioC.mid/tracks/track0.csv", 1);
+            String preludio = readFile(1, "../midiParser/assets/midi/csv/preludioC.mid/tracks/track0.csv");
             randomProjections(preludio, 12, 3, 4, 3);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String readFile(String path, int column) throws IOException {
+    private static String readFile(int column, String path) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get(path), Charset.forName("UTF-8"));
         List<String> parsedColumn = new ArrayList<>(lines.size());
         for (String line : lines) {
